20be7275923bd4bd049b024a27f75368359888ca
Lee Ruben Valero <vleeruben@gmail.com>
2025-11-17 00:24:36 +0800

    db connected
    Connected with PDO

--- db_connect.php
+++ db_connect.php
@@ -3,14 +3,13 @@ $hostname = "localhost";
 $username = "root";
 $password = "";
 $database = "auth";
-$conn = "";
 
 try {
-    $conn = mysqli_connect($hostname, $username, $password, $database);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-} catch (mysqli_sql_exception $e) {
+    $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Database connected successfully!";
+} catch (PDOException $e) {
     die("Could not connect to the database: " . $e->getMessage());
 }
 ?>
